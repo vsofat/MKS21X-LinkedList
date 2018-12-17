20484a35794c1b4b7dbade13792e8d15e87be603
vsofat <vsofat00@stuy.edu>
2018-12-17 02:41:07 -0500

    size added

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -11,11 +11,14 @@ public class MyLinkedList{
  private Node getNthNode (int n){
    Node current = start;
    for (int i = 0; i < n; i++){
-     current = current.next(); // loop finds the node at int n by looping through the list 
+     current = current.next(); // loop finds the node at int n by looping through the list
    }
    return current;
  }
 
+ public int size(){
+   return length;
+ }
  public boolean add (Integer value){
 
  }
